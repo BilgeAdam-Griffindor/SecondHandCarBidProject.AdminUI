73a6f1629e2aa4118e2e726a83ade7a0fab1497b
Endalth <alican__karamaras@hotmail.com>
2022-10-04 23:39:37 +0300

    CarController and DTO layer.

---
+++ SecondHandCarBidProject.AdminUI.GUI/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecondHandCarBidProject.AdminUI.GUI.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
