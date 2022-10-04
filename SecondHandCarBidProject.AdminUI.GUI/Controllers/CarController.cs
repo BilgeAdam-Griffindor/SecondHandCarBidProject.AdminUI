37a0fe068aba087fca9af74aa5e57bee72e0ac4d
Endalth <alican__karamaras@hotmail.com>
2022-10-05 00:06:03 +0300

    DTO layer.

--- SecondHandCarBidProject.AdminUI.GUI/Controllers/CarController.cs
+++ SecondHandCarBidProject.AdminUI.GUI/Controllers/CarController.cs
@@ -4,6 +4,7 @@ namespace SecondHandCarBidProject.AdminUI.GUI.Controllers
 {
     public class CarController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
