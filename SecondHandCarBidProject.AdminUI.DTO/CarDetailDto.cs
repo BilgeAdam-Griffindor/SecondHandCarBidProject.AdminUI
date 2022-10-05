43cf556985ca3d44ec4751ad3532649c92fbecc8
Recai-Capkin <recai.capkin28@gmail.com>
2022-10-05 11:45:27 +0300

    Car detail eklenmeye başlandı.

---
+++ SecondHandCarBidProject.AdminUI.DTO/CarDetailDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SecondHandCarBidProject.AdminUI.DTO
+{
+    public class CarDetailDto
+    {
+        public List<SelectListItem> BireyselKurumsalListe { get; set; }
+        public int BireyselKurumsalId { get; set; }
+
+        public string description { get; set; }
+        public string SelectedItem { get; set; }
+        public List<SelectListItem> selectItemList
+        {
+            get; set;
+        }
+    }
+}
