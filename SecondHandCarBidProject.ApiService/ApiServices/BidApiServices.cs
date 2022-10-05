8ea41d6d097e18e87ce19c80acb6c6813ab06e93
ernkny <eren.kinay.98@windowslive.com>
2022-10-05 11:28:35 +0300

    Admin Layout, 'BidAdd' page added and 'bidadd' api servis added to project

---
+++ SecondHandCarBidProject.ApiService/ApiServices/BidApiServices.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using SecondHandCarBidProject.ApiService.Dto_s;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.ApiService.ApiServices
+{
+    public class BidApiServices
+    {
+        HttpClient _client;
+        public BidApiServices(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<bool> BiddAddAsync(BidAddDTO bidAddDto)
+        {
+            var body = new StringContent(JsonConvert.SerializeObject(bidAddDto));
+            body.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            // WARNING: "Api/Url"  is formal URL. When Api service be ready  Url will change
+            var response = await _client.PostAsync("Api/Url", body);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                bool.TryParse(content, out bool result);
+                return result;
+            }
+
+            return false;
+        }
+    }
+}
