3094603e5b59ebb8e4e63f5e5244de9702954963
Endalth <alican__karamaras@hotmail.com>
2022-10-05 11:58:32 +0300

    Car List View

---
+++ SecondHandCarBidProject.AdminUI.DTO/CarListTableRowDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.AdminUI.DTO
+{
+    public class CarListTableRowDTO
+    {
+        public int CarId { get; set; }
+        public string CarBrandName { get; set; }
+        public string CarModelName { get; set; }
+        public string Status { get; set; }
+        public string UserName { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
---
+++ SecondHandCarBidProject.AdminUI.GUI/ViewModels/CarListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SecondHandCarBidProject.AdminUI.DTO;
+
+namespace SecondHandCarBidProject.AdminUI.GUI.ViewModels
+{
+    public class CarListViewModel
+    {
+        public int? BrandId { get; set; }
+        public List<SelectListItem> BrandList { get; set; }
+
+        public int? ModelId { get; set; }
+        public List<SelectListItem> ModelList { get; set; }
+
+        public int? StatusId { get; set; }
+        public List<SelectListItem> StatusList { get; set; }
+
+        public List<CarListTableRowDTO> CarTableRows { get; set; }
+
+        public int Page { get; set; }
+        public int ItemPerPage { get; set; }
+    }
+}
