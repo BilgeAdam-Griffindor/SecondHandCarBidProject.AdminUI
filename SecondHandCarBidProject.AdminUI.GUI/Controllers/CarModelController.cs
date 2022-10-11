a5503723cbb622f3c71aa87824908f64d527e8e4
emre çakmak <45544939+EmreCkm98@users.noreply.github.com>
2022-10-11 09:48:32 +0300

    controllers and views added for database tables

---
+++ SecondHandCarBidProject.AdminUI.GUI/Controllers/CarModelController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondHandCarBidProject.AdminUI.DTO.AuthorizationDtos;
+
+namespace SecondHandCarBidProject.AdminUI.GUI.Controllers
+{
+    public class CarModelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CarModelAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CarModelAdd(RolePageActionAuthAddDto data)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CarModelUpdate(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CarModelUpdate(RolePageActionAuthAddDto data)
+        {
+            return View();
+        }
+    }
+}
