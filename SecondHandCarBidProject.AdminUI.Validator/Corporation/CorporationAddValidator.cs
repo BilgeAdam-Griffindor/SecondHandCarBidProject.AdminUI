577c0613c49cd7273f7663104c17b79c7e539058
Essrinc <sabireesraozcan@gmail.com>
2022-10-16 23:12:00 +0300

    validations

---
+++ SecondHandCarBidProject.AdminUI.Validator/Corporation/CorporationAddValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using SecondHandCarBidProject.AdminUI.DTO.CorporationDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.AdminUI.Validator.Corporation
+{
+    public class CorporationAddValidator: AbstractValidator<CorporationAddDTO>
+    {
+        public CorporationAddValidator()
+        {
+            RuleFor(x => x.CompanyName).NotEmpty();
+            RuleFor(x => x.AddressInfoId).NotEmpty();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.CorporationTypeId).NotEmpty();
+            RuleFor(x => x.CorporatePackageTypeId).NotEmpty();
+        }
+    }
+}
---
+++ SecondHandCarBidProject.AdminUI.Validator/NotificationMessage/NotificationMessageAddValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SecondHandCarBidProject.AdminUI.DTO.NotificationDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.AdminUI.Validator.NotificationMessage
+{
+    public class NotificationMessageAddValidator:AbstractValidator<NotificationMessageAddDTO>
+    {
+        public NotificationMessageAddValidator()
+        {
+            RuleFor(x => x.Content).NotEmpty();
+
+        }
+    }
+}
